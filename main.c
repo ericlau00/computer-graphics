e1593f99aa40b88438c74aea91c9846d2a8ae1ed
ericlau00 <ericlau41802@gmail.com>
2020-02-13 09:25:06 -0500

    Add boiler plate

--- main.c
+++ main.c
@@ -10,9 +10,28 @@
 int main() {
 
   screen s;
-  struct matrix *edges;
 
-  edges = new_matrix(4, 4);
+  struct matrix *m2;
+  struct matrix *m1;
 
-  free_matrix( edges );
-}  
+  m1 = new_matrix(4, 4);
+  m2 = new_matrix(4, 4);
+
+  printf("Testing add_edge. Adding (1, 2, 3), (4, 5, 6) m2 =\n");
+
+  add_edge(m2, 1, 2, 3, 4, 5, 6);
+  print_matrix(m2);
+
+  printf("Testing ident. m1 =\n");
+
+  ident(m1);
+  print_matrix(m1);
+
+  printf("Testing Matrix mult. m1 * m2 =\n");
+
+  printf("Testing Matrix mult. m1=\n");
+
+  printf("Testing Matrix mult. m1 * m2 = \n");
+
+  free_matrix(m2);
+}
